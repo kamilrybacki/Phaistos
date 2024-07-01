d84767e759b945c63ac820129f89580f184ab4f3
Kamil Rybacki <kamilandrzejrybacki@gmail.com>
2024-07-01 14:07:34 +0200

    swuitch to setup py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+import setuptools  # type: ignore
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+if version := os.environ.get("RELEASE_VERSION"):
+    setuptools.setup(
+        name="phaistos",
+        version=version,
+        author="Kamil Rybacki",
+        author_email="kamilandrzejrybacki@gmail.com",
+        description="Tooling to automatically generate validated data models from YAML configuration files",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://phaistos.readthedocs.io/en/latest",
+        packages=setuptools.find_packages(),
+        install_requires=[
+            "PyYAML==6.0.1",
+            "pydantic==2.7.0",
+        ]
+    )
+else:
+    raise RuntimeError("RELEASE_VERSION environment variable is not set")
